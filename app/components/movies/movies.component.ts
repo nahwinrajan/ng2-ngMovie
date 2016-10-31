29b1039107be4200a63eb4f10d619c1741d8db75
nahwinrajan <nahwin.rajan@gmail.com>
2016-11-01 04:08:21 +0700

    finish add services

--- app/components/movies/movies.component.ts
+++ app/components/movies/movies.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component }      from '@angular/core';
+import { MovieService }   from '../../services/movie.service';
 
 @Component({
   moduleId: module.id,
   selector: 'movies',
-  templateUrl: 'movies.component.html'
+  templateUrl: 'movies.component.html',
+  providers: [MovieService]
 })
 
-export class MoviesComponent { }
+export class MoviesComponent {
+  listPopularMovies:Array<Object>;
+  listTheaters:Array<Object>;
+
+  constructor(private _movieService: MovieService) {
+    this._movieService.getPopular().subscribe(res => {
+      this.listPopularMovies = res.results;
+    })
+
+    this._movieService.getInTheaters().subscribe(res => {
+      this.listTheaters = res.results;
+    })
+  }
+}
---
+++ app/services/movie.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class MovieService{
+  apiKey:string;
+
+  constructor(private _jsonp:Jsonp) {
+    this.apiKey = '91de4694a504cf666081a5bbaafa3d79';
+    console.log('movie service ready...');
+  }
+
+  getPopular(){
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key=' + this.apiKey)
+      .map(res => res.json())
+  }
+
+  getInTheaters() {
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2016-10-01&primary_release_date.lte=2016-10-31&api_key=' + this.apiKey)
+      .map(res => res.json())
+  }
+}
